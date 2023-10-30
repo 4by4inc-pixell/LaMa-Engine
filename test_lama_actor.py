6b7b0f38b10926b644a92dd222c2622eaac107b1
HyeongSeok Kim <tiryul@gmail.com>
2023-10-30 17:00:09 +0900

    update: test_lama_actor.py에 input shape test case 추가

--- test_lama_actor.py
+++ test_lama_actor.py
@@ -9,21 +9,28 @@ import numpy as np
 
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), "configs", "predict_config.yaml")
 CUDA_VISIBLE_DEVICES = torch.cuda.device_count()
-NUMBER_OF_ACTOR_PER_GPU = 2
+NUMBER_OF_ACTOR_PER_GPU = 1
 NUMBER_OF_TOTAL_ACTOR = NUMBER_OF_ACTOR_PER_GPU * CUDA_VISIBLE_DEVICES
 ACTOR_NUM_GPUS = CUDA_VISIBLE_DEVICES / NUMBER_OF_ACTOR_PER_GPU
-VIDEO_RESOLUTIONS = {
+TARGET_RESOLUTIONS_FOR_DYNAMIC_INPUT_TEST = {
     "SD": (480, 640, 3),
     "HD": (720, 1280, 3),
     "FHD": (1080, 1920, 3),
     "2K": (1440, 2560, 3),
-    # "4K": (2160, 3840, 3), # NOT SUPPORT!
+    "2K.T": (1440, 2560, 3),
+    "FHD.T": (1920, 1080, 3),
+    "HD.T": (1280, 720, 3),
+    "SD.T": (640, 480, 3),
+    # "4K": (2160, 3840, 3),  # NOT SUPPORT!
     # "8K": (4320, 7680, 3), # NOT SUPPORT!
 }
 
 
 class TestLaMaActor(unittest.TestCase):
     def setUp(self) -> None:
+        """
+        모든 unittest 직전에 이 메서드가 호출됩니다.
+        """
         self.actors = [
             LaMaActor.options(num_gpus=ACTOR_NUM_GPUS).remote(config_path=CONFIG_PATH)
             for _ in range(NUMBER_OF_TOTAL_ACTOR)
@@ -51,6 +58,9 @@ class TestLaMaActor(unittest.TestCase):
         ]
 
     def tearDown(self) -> None:
+        """
+        모든 unittest 직후에 이 메서드가 호출됩니다.
+        """
         _ = [ray.kill(actor) for actor in self.actors]
 
     def test_single_actor_solid(self):
@@ -66,8 +76,8 @@ class TestLaMaActor(unittest.TestCase):
 
     def test_single_actor_dynamic(self):
         __method__ = "test_single_actor_dynamic"
-        for key in VIDEO_RESOLUTIONS:
-            input_shape = VIDEO_RESOLUTIONS[key]
+        for key in TARGET_RESOLUTIONS_FOR_DYNAMIC_INPUT_TEST:
+            input_shape = TARGET_RESOLUTIONS_FOR_DYNAMIC_INPUT_TEST[key]
             image = np.clip(np.random.random(input_shape) * 255, 0, 255).astype(
                 np.uint8
             )
@@ -78,6 +88,29 @@ class TestLaMaActor(unittest.TestCase):
             result = ray.get(result_remote)
             print(
                 f"{__method__}::{key} [image shape={image.shape} | mask shape={mask.shape} | result shape={result.shape}] done."
+            )
+
+    def test_single_actor_processing_time(self):
+        __method__ = "test_single_actor_processing_time"
+        from tqdm import tqdm
+        import time
+
+        number_of_iteration = 100
+        for key in TARGET_RESOLUTIONS_FOR_DYNAMIC_INPUT_TEST:
+            input_shape = TARGET_RESOLUTIONS_FOR_DYNAMIC_INPUT_TEST[key]
+            image = np.clip(np.random.random(input_shape) * 255, 0, 255).astype(
+                np.uint8
+            )
+            mask = np.clip(np.random.random(input_shape[:-1]) * 255, 0, 255).astype(
+                np.uint8
+            )
+            t = time.time()
+            for i in tqdm(range(number_of_iteration)):
+                result_remote = self.actors[0].run.remote(image, mask)
+                result = ray.get(result_remote)
+            t = time.time() - t
+            print(
+                f"{__method__}::{key} [image shape={image.shape} | mask shape={mask.shape} | result shape={result.shape} | avg proc time = {number_of_iteration/t} inference/sec] done."
             )
 
     def test_multi_actor_solid(self):
@@ -97,14 +130,22 @@ class TestLaMaActor(unittest.TestCase):
         __method__ = "test_multi_actor_dynamic"
         inputs = [
             (
-                np.clip(np.random.random(VIDEO_RESOLUTIONS[key]) * 255, 0, 255).astype(
-                    np.uint8
-                ),
                 np.clip(
-                    np.random.random(VIDEO_RESOLUTIONS[key][:-1]) * 255, 0, 255
+                    np.random.random(TARGET_RESOLUTIONS_FOR_DYNAMIC_INPUT_TEST[key])
+                    * 255,
+                    0,
+                    255,
+                ).astype(np.uint8),
+                np.clip(
+                    np.random.random(
+                        TARGET_RESOLUTIONS_FOR_DYNAMIC_INPUT_TEST[key][:-1]
+                    )
+                    * 255,
+                    0,
+                    255,
                 ).astype(np.uint8),
             )
-            for key in VIDEO_RESOLUTIONS
+            for key in TARGET_RESOLUTIONS_FOR_DYNAMIC_INPUT_TEST
         ]
         results = self.actor_pool.map(
             lambda actor, v: actor.run.remote(v[0], v[1]), inputs
