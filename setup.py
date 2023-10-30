cbeab08a4626d86ec2a1eb89637fd6a128da38e6
HyeongSeok Kim <tiryul@gmail.com>
2023-10-30 19:07:53 +0900

    add: setup.py 추가

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+setup script for pip install
+"""
+
+from setuptools import setup, find_packages
+
+
+REQUIREMENTS = [
+    "torch",
+    "torchvision",
+    "albumentations==0.5.2",
+    "onnx",
+    "omegaconf",
+    "webdataset",
+    "tqdm",
+    "easydict",
+    "pandas",
+    "lightning",
+    "kornia ==0.5.0",
+    "scikit-learn",
+    "ray==2.7.1",
+]
+
+INCLUDE_MODULE = ["lama_engine"]
+
+
+setup(
+    name="lama-engine",
+    version="0.1.0",
+    packages=find_packages(include=INCLUDE_MODULE),
+    install_requires=REQUIREMENTS,
+)
