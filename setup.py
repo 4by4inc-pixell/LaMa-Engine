74587581d96b93b6308991407839e8dc4e9c145c
HyeongSeok Kim <tiryul@gmail.com>
2023-10-31 11:53:56 +0900

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 setup script for pip install
 """
-
+import os
 from setuptools import setup, find_packages
 
 
@@ -21,13 +21,23 @@ REQUIREMENTS = [
     "ray==2.7.1",
 ]
 
+
+def copy_dir():
+    dir_path = "YOUR_DIR_HERE"
+    base_dir = os.path.join("MODULE_DIR_HERE", dir_path)
+    for dirpath, dirnames, files in os.walk(base_dir):
+        for f in files:
+            yield os.path.join(dirpath.split("/", 1)[1], f)
+
+
 # INCLUDE_MODULE = ["lama_engine", "lama_engine.models", "lama_engine.saicinpainting"]
 
 
 setup(
     name="lama-engine",
-    version="0.3.4",
-    package_dir={"lama_engine": "lama_engine"},
+    version="0.3.5",
+    # package_dir={"lama_engine": "lama_engine"},
     packages=find_packages(),
     install_requires=REQUIREMENTS,
+    include_package_data=True,
 )
